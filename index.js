5529f1c22e582964d99bf94a1acb53bb9b72a7db
db Dev-Omar db <ungober@gmail.com>
2022-02-15 11:51:11 -0600

    agregamos script de fire stone :octocat:

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-console.log("works")
+ // Import the functions you need from the SDKs you need
+ import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.6/firebase-app.js";
+ 
+ // TODO: Add SDKs for Firebase products that you want to use
+ // https://firebase.google.com/docs/web/setup#available-libraries
+
+ // Your web app's Firebase configuration
+ const firebaseConfig = {
+   apiKey: "AIzaSyBHs20BFkmRC2tA1hcn8JQ_5EIZK3yoz0E",
+   authDomain: "fir-js-crud-75a73.firebaseapp.com",
+   projectId: "fir-js-crud-75a73",
+   storageBucket: "fir-js-crud-75a73.appspot.com",
+   messagingSenderId: "38358800655",
+   appId: "1:38358800655:web:ae13961a94c5cfe66b067a"
+ };
+
+ // Initialize Firebase
+ const app = initializeApp(firebaseConfig);
+
+
+
+
+ console.log("omra")
